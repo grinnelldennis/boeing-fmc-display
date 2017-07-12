36b1741068add284e4da125f69ba21edde12f4a1
grinnelldennis <chanchun@grinnell.edu>
2017-07-12 12:06:53 -0500

    created page for pos init

--- Kernel.java
+++ Kernel.java
@@ -4,8 +4,12 @@ class Kernel {
   public static void main(String[] args) throws FileNotFoundException{
     Aircraft ac = new Aircraft("77w");
     Navigation nv = new Navigation();
-    Page ident = new Page("Ident", ac, nv);
-    ident.renderScreen();
-  }
+    World wr = new World();
 
+    Page ident = new Page("Ident", ac, nv, wr);
+    ident.renderScreen();
+
+    Page posInit = new Page("Pos-Init", ac, nv, wr);
+    posInit.renderScreen();
+  }
 }
