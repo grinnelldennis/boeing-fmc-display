84770227c8feb190d6903ace669418512ad0c8fb
grinnelldennis <chanchun@grinnell.edu>
2017-07-12 12:08:44 -0500

    added attributes

--- Navigation.java
+++ Navigation.java
@@ -7,11 +7,16 @@ class Navigation {
   public Navigation() {
     attributes = new HashMap<>();
     loadAttributes();
+    System.out.println ("Navigation contains " + attributes.size() + " items.");
   }
 
   public void loadAttributes() {
     attributes.put("NV-DCYCLE", "AIRAC-1601");
     attributes.put("NV-ACTIVE", "JAN07FEB03/16");
-    System.out.println ("Navigation HashMap contains " + attributes.size() + " items.");
+
+    attributes.put("NV-POSNSG", "N48D36.2"); //GPS positiion, latitude
+    attributes.put("NV-POSEWG", "W123D01.1"); //GPS position, longitude
+    attributes.put("NV-POSNSL", "N48D36.1"); //last positiion, latitude
+    attributes.put("NV-POSEWL", "W123D01.1"); //last position, longitude
   }
 }
